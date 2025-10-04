9bdd783d31b6407eec5ef58ba58691e18e40bf53
viswaswathi4b9-star <viswaswathi4b9@gmail.com>
2025-10-04 09:54:38 +0530

    4102025

---
+++ 6-branch.groovy
@@ -0,0 +1,25 @@
+
+pipeline{
+    agent any
+    stages{
+        stage('build'){
+            steps{
+                echo "build pipleline"
+            }
+
+        }
+        stage('test'){
+            steps{
+                echo "test pipleline"
+            }
+
+        }
+        stage('docker'){
+            steps{
+                echo "docker"
+            }
+
+        }
+    }
+
+}
